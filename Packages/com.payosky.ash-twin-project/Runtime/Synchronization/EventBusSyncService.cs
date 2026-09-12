968d639faea586612aa27d524ce04905d054d8f8
Juan Payán <juandavidpf@proton.me>
2026-09-12 00:06:28 -0500

    feat: Add SyncServices

---
+++ Packages/com.payosky.ash-twin-project/Runtime/Synchronization/EventBusSyncService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using AshTwinProject.Core;
+using NomaiFramework.EventBus;
+using NomaiFramework.Services;
+using NomaiFramework.StateManagement;
+using Unity.Netcode;
+
+namespace AshTwinProject.Synchronization
+{
+    /// <summary>
+    ///     This service integrates with Unity's Netcode for GameObjects to propagate specific events
+    ///     to clients when those events are raised on the server side.
+    /// </summary>
+    public class EventBusSyncService : NetworkBehaviour, IService
+    {
+        public virtual Type TypeSignature => typeof(EventBusSyncService);
+        private readonly Dictionary<IStateMachine, NetworkVariable<NetworkData4096>> _stateMachinesBound = new();
+        private readonly Dictionary<IStateMachine, NetworkVariable<NetworkData4096>.OnValueChangedDelegate> _clientSyncListeners = new();
+
+        public override void OnNetworkSpawn()
+        {
+            ServiceLocator.AddService(this);
+            ServiceLocator.GetService<EventBusService>().OnEventDispatched += HandleRaisedEvent;
+        }
+
+        public void HandleRaisedEvent(IEvent raisedEvent)
+        {
+            if (!IsServer || raisedEvent is not ISyncedEvent syncedEvent) return;
+
+            switch (syncedEvent.GetNetworkRepresentation()) {
+                case NetworkData32 networkData32:
+                    HandleRaisedEventClientRPC(networkData32);
+                    return;
+                case NetworkData64 networkData64:
+                    HandleRaisedEventClientRPC(networkData64);
+                    return;
+                case NetworkData128 networkData128:
+                    HandleRaisedEventClientRPC(networkData128);
+                    return;
+                case NetworkData512 networkData512:
+                    HandleRaisedEventClientRPC(networkData512);
+                    return;
+                case NetworkData4096 networkData4096:
+                    HandleRaisedEventClientRPC(networkData4096);
+                    return;
+            }
+        }
+
+        [ClientRpc]
+        private void HandleRaisedEventClientRPC(NetworkData32 networkEventData)
+        {
+            if (IsServer) return;
+            ISyncedEvent serverEvent = networkEventData.ToObject<ISyncedEvent>();
+            ServiceLocator.GetService<EventBusService>().Trigger(serverEvent);
+        }
+
+        [ClientRpc]
+        private void HandleRaisedEventClientRPC(NetworkData64 networkEventData)
+        {
+            if (IsServer) return;
+            ISyncedEvent serverEvent = networkEventData.ToObject<ISyncedEvent>();
+            ServiceLocator.GetService<EventBusService>().Trigger(serverEvent);
+        }
+
+        [ClientRpc]
+        private void HandleRaisedEventClientRPC(NetworkData128 networkEventData)
+        {
+            if (IsServer) return;
+            ISyncedEvent serverEvent = networkEventData.ToObject<ISyncedEvent>();
+            ServiceLocator.GetService<EventBusService>().Trigger(serverEvent);
+        }
+
+        [ClientRpc]
+        private void HandleRaisedEventClientRPC(NetworkData512 networkEventData)
+        {
+            if (IsServer) return;
+            ISyncedEvent serverEvent = networkEventData.ToObject<ISyncedEvent>();
+            ServiceLocator.GetService<EventBusService>().Trigger(serverEvent);
+        }
+
+        [ClientRpc]
+        private void HandleRaisedEventClientRPC(NetworkData4096 networkEventData)
+        {
+            if (IsServer) return;
+            ISyncedEvent serverEvent = networkEventData.ToObject<ISyncedEvent>();
+            ServiceLocator.GetService<EventBusService>().Trigger(serverEvent);
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            ServiceLocator.RemoveService(this);
+            if (ServiceLocator.TryGetService(out EventBusService eventService)) {
+                eventService.OnEventDispatched -= HandleRaisedEvent;
+            }
+        }
+    }
+}
---
+++ Packages/com.payosky.ash-twin-project/Runtime/Synchronization/StateMachineSyncService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using AshTwinProject.Core;
+using Cysharp.Threading.Tasks;
+using NomaiFramework.Services;
+using NomaiFramework.StateManagement;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace AshTwinProject.Synchronization
+{
+    /// <summary>
+    ///     Provides functionality for synchronizing state machines across networked environments.
+    /// </summary>
+    public class StateMachineSyncService : NetworkBehaviour, IService
+    {
+        public virtual Type TypeSignature => typeof(StateMachineSyncService);
+        private readonly Dictionary<IStateMachine, NetworkVariable<NetworkData4096>> _stateMachinesBound = new();
+        private readonly Dictionary<IStateMachine, NetworkVariable<NetworkData4096>.OnValueChangedDelegate> _clientSyncListeners = new();
+
+        public override void OnNetworkSpawn()
+        {
+            ServiceLocator.AddService(this);
+        }
+
+        public void OnServerStateChanged(IStateMachine stateMachine, IState _, IState newState)
+        {
+            if (!IsServer) return;
+            if (newState is not ISyncedState syncedState || !_stateMachinesBound.TryGetValue(stateMachine, out NetworkVariable<NetworkData4096> networkVariable)) return;
+            networkVariable.Value = syncedState.To4096();
+#if DEBUG
+            Debug.Log($"Requesting ServerState Sync: {newState.GetType().Name}\nPayload: {networkVariable.Value.DataPayload.Value}");
+#endif
+        }
+
+        public void BindStateMachine(IStateMachine stateMachine, NetworkVariable<NetworkData4096> syncedState, BindingMode bindingMode = BindingMode.OnStateChangeRequested)
+        {
+            if (!_stateMachinesBound.TryAdd(stateMachine, syncedState)) return;
+
+            if (IsServer) {
+                switch (bindingMode) {
+                    case BindingMode.OnStateChangeRequested:
+                    default:
+                        stateMachine.OnStateChangeRequested += OnServerStateChanged;
+                        break;
+                    case BindingMode.OnStateChanged:
+                        stateMachine.OnStateChanged += OnServerStateChanged;
+                        break;
+                }
+            }
+            else {
+                NetworkVariable<NetworkData4096>.OnValueChangedDelegate clientDelegate = (_, serverState) =>
+                {
+#if DEBUG
+                    Debug.Log($"ClientState Sync Requested: {serverState.DataType.Value}\nPayload: {serverState.DataPayload.Value}");
+#endif
+                    stateMachine.SetState(serverState.ToObject<ISyncedState>()).Forget();
+                };
+
+                _clientSyncListeners.Add(stateMachine, clientDelegate);
+                if (syncedState.Value.HasData) {
+                    clientDelegate(syncedState.Value, syncedState.Value);
+                }
+
+                syncedState.OnValueChanged += clientDelegate;
+            }
+        }
+
+        public void UnbindStateMachine(IStateMachine stateMachine)
+        {
+            if (!_stateMachinesBound.Remove(stateMachine, out NetworkVariable<NetworkData4096> syncedState)) return;
+
+            if (IsServer) {
+                stateMachine.OnStateChangeRequested -= OnServerStateChanged;
+                stateMachine.OnStateChanged -= OnServerStateChanged;
+            }
+            else if (_clientSyncListeners.TryGetValue(stateMachine, out NetworkVariable<NetworkData4096>.OnValueChangedDelegate listener)) {
+                syncedState.OnValueChanged -= listener;
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            ServiceLocator.RemoveService(this);
+
+            foreach (IStateMachine stateMachine in new List<IStateMachine>(_stateMachinesBound.Keys)) {
+                UnbindStateMachine(stateMachine);
+            }
+
+            _stateMachinesBound.Clear();
+        }
+
+        public enum BindingMode
+        {
+            OnStateChangeRequested,
+            OnStateChanged
+        }
+    }
+}
